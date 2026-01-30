2ded0f185a9128f6e692e30f9f0b0afc7e769952
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2026-01-30 13:11:27 +0100

    Tutorial Inicio Angular

--- first-app/.angular/cache/21.1.0/first-app/vite/deps/@angular_core.js
+++ first-app/.angular/cache/21.1.0/first-app/vite/deps/@angular_core.js
@@ -501,7 +501,7 @@ import {
   ɵɵvalidateAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-ZGAW3EZV.js";
+} from "./chunk-QIFU6WZK.js";
 export {
   ANIMATION_MODULE_TYPE,
   APP_BOOTSTRAP_LISTENER,
--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -1,20 +1,24 @@
-import {Component} from '@angular/core';
-import {Home} from './home/home';
+import { Component } from "@angular/core";
+import { Home } from "./home/home";
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [Home],
+  selector: "app-root",
+  imports: [Home, RouterOutlet, RouterLink],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
-        <app-home />
+        <router-outlet />
       </section>
     </main>
-  `,  styleUrls: ['./app.css'],
+  `,
+  styleUrls: ["./app.css"],
 })
 export class App {
-  title = 'homes';
+  title = "homes";
 }
